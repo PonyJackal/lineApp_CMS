c9aa7ac00e9f96b7bb673c43811bda58dbc44ef4
Pony <pony1125@outlook.com>
2020-08-22 07:25:18 -0400

    Remove headers in api'

--- src/api/api.js
+++ src/api/api.js
@@ -9,10 +9,10 @@ const makeAPI = () => {
         baseURL: 'http://beta3-lineapp.simplyintense.com/api/v1',
         headers: {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
+            // 'Access-Control-Allow-Origin': '*',
             'Authorization': token ? `${token}` : '',
-            'Access-Control-Allow-Headers': 'origin, content-type',
-            'Access-Control-Allow-Credentials': true
+            // 'Access-Control-Allow-Headers': 'origin, content-type',
+            // 'Access-Control-Allow-Credentials': true
         },
     })
 
